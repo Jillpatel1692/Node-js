14b57287d608a353c05dabe170aa7524e4356893
“[jill <[jillpatel1692@gmail.com]>
2022-07-10 11:51:15 +0530

    initail commit

--- node js/fundamentals/fundamental.js
+++ node js/fundamentals/fundamental.js
@@ -1,15 +1,15 @@
 // export import variables
-// const exportedVariables = require('./export-variable');
-// console.log('Exported function from other file : ',exportedVariables.sumXY());
+const exportedVariables = require('./export-variable');
+console.log('Exported function from other file : ',exportedVariables.sumXY());
 
 // Core Modules ------------------------------------------------------------------------------
 
 // Global Core Modules
-// console.log('\nCurrent Directory: ', __dirname);
-// console.log('Current File Location: ', __filename);
+console.log('\nCurrent Directory: ', __dirname);
+console.log('Current File Location: ', __filename);
 
 // -> Non Global Core Modules
-// const fsVariable = require('fs');
-// fsVariable.writeFileSync('Non-Global.txt','Example of Non-Global Core Module...!');
-// console.log('\nNon-Global Core Module txt written.');
+const fsVariable = require('fs');
+fsVariable.writeFileSync('Non-Global.txt','Example of Non-Global Core Module...!');
+console.log('\nNon-Global Core Module txt written.');
 
