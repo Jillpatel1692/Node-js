f5b7e7e84bd4a48d2e8af427eb8b9fe555147788
“[jill <[jillpatel1692@gmail.com]>
2022-07-16 11:15:10 +0530

    commit

--- Notes app/pages.js
+++ Notes app/pages.js
@@ -3,25 +3,25 @@ const fs = require('fs');
 const { title } = require('process');
 
 http.createServer(function (req, res) {
-  // fs.open('notepad.txt', 'w', function (err, file) {
-  //   if (err) {
-  //         throw err;}
-  //       else{
-  //         res.writeHead(200, {'Content-Type': 'text/html'});
-  //         res.write('File created');
-  //         return res.end();}
-  // });
-  // fs.readFile('notepad.txt', function(err, data) {
-  //   if(err){
-  //     res.writeHead(200, {'Content-Type': 'text/html'});
-  //     res.write('Error reading file...');
-  //     return res.end();
-  //   }else{
-  //     res.writeHead(200, {'Content-Type': 'text/html'});
-  //      res.write(data);
-  //     return res.end();
-  //   }
-  // });
+  fs.open('notepad.txt', 'w', function (err, file) {
+    if (err) {
+          throw err;}
+        else{
+          res.writeHead(200, {'Content-Type': 'text/html'});
+          res.write('File created');
+          return res.end();}
+  });
+  fs.readFile('notepad.txt', function(err, data) {
+    if(err){
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write('Error reading file...');
+      return res.end();
+    }else{
+      res.writeHead(200, {'Content-Type': 'text/html'});
+       res.write(data);
+      return res.end();
+    }
+  });
   fs.unlink('notepad.txt', function (err) {
     if (err) {
           throw err;}
@@ -30,13 +30,13 @@ http.createServer(function (req, res) {
           res.write('File Deleted');
           return res.end();}
   });
-  // fs.rename('mynewfile5.txt', 'notepad2.txt', function (err) {
-  //   if (err) {
-  //             throw err;}
-  //           else{
-  //             res.writeHead(200, {'Content-Type': 'text/html'});
-  //             res.write('File renamed');
-  //             return res.end();}
-  // });
+  fs.rename('mynewfile5.txt', 'notepad2.txt', function (err) {
+    if (err) {
+              throw err;}
+            else{
+              res.writeHead(200, {'Content-Type': 'text/html'});
+              res.write('File renamed');
+              return res.end();}
+  });
   
 }).listen(8000);
